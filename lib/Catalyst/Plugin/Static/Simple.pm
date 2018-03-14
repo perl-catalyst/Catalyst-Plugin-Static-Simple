9a4b0bc2417b66931e3eaee04450c707e584cac2
Dagfinn Ilmari Mannsåker <ilmari@ilmari.org>
2018-03-14 12:07:45 +0000

    Release 0.35

--- lib/Catalyst/Plugin/Static/Simple.pm
+++ lib/Catalyst/Plugin/Static/Simple.pm
@@ -9,7 +9,7 @@ use MooseX::Types::Moose qw/ArrayRef Str/;
 use Catalyst::Utils;
 use namespace::autoclean;
 
-our $VERSION = '0.34';
+our $VERSION = '0.35';
 
 has _static_file => ( is => 'rw' );
 has _static_debug_message => ( is => 'rw', isa => ArrayRef[Str] );
